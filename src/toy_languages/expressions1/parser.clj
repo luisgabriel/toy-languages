c69ebf02582c58f3060d76ea5de6d62bf6c312d7
Luís Gabriel Lima <lampih@gmail.com>
2014-05-18 14:00:40 -0300

    E1: Extracting the grammar from the parse function

--- src/toy_languages/expressions1/parser.clj
+++ src/toy_languages/expressions1/parser.clj
@@ -1,43 +1,43 @@
 (ns toy-languages.expressions1.parser
   (:require [instaparse.core :as insta]))
 
-(def parser
-  (insta/parser
-    "program = exp
+(def grammar
+  "program = exp
 
-     <exp> = cexp | unary-exp | binary-exp
+   <exp> = cexp | unary-exp | binary-exp
 
-     <cexp> = value | <'('> exp <')'>
+   <cexp> = value | <'('> exp <')'>
 
-     value = int | bool | string
+   value = int | bool | string
 
-     unary-exp = minus cexp
-               | not cexp
-               | length cexp
+   unary-exp = minus cexp
+             | not cexp
+             | length cexp
 
-     binary-exp = exp plus cexp
-                | exp minus cexp
-                | exp and cexp
-                | exp or cexp
-                | exp eq cexp
-                | exp concat cexp
+   binary-exp = exp plus cexp
+              | exp minus cexp
+              | exp and cexp
+              | exp or cexp
+              | exp eq cexp
+              | exp concat cexp
 
-     int = int-literal
-     bool = bool-literal
-     string = string-literal
-     not = <'not'>
-     length = <'length'>
-     minus = <'-'>
-     plus = <'+'>
-     and = <'and'>
-     or = <'or'>
-     eq = <'=='>
-     concat = <'++'>
+   int = int-literal
+   bool = bool-literal
+   string = string-literal
+   not = <'not'>
+   length = <'length'>
+   minus = <'-'>
+   plus = <'+'>
+   and = <'and'>
+   or = <'or'>
+   eq = <'=='>
+   concat = <'++'>
 
-     int-literal = #'\\d+'
-     bool-literal = 'true' | 'false'
-     <string-literal> = <'\"'> #'[A-Za-z0-9]+' <'\"'>"
-    :auto-whitespace :standard))
+   int-literal = #'\\d+'
+   bool-literal = 'true' | 'false'
+   <string-literal> = <'\"'> #'[A-Za-z0-9]+' <'\"'>")
+
+(def parser (insta/parser grammar :auto-whitespace :standard))
 
 (def transform-options
   {:int-literal read-string
