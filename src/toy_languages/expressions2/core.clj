f97ce8455602a9a5da53e4d42400bc23c6e3e36f
Luís Gabriel Lima <luis.gabriel@openbossa.org>
2014-05-19 19:36:14 -0300

    Adding the exec function

---
+++ src/toy_languages/expressions2/core.clj
@@ -0,0 +1,14 @@
+(ns toy-languages.expressions2.core
+  (:require [toy-languages.expressions2.parser :as parser])
+  (:require [toy-languages.expressions2.typechecker :as typechecker])
+  (:require [toy-languages.expressions2.interpreter :as interpreter]))
+
+(defn exec [input]
+  (let [result (parser/parse input)
+        parsed (first result)
+        ast-or-failure (nth result 1)]
+    (if parsed
+      (if (typechecker/check ast-or-failure)
+        (interpreter/evaluate ast-or-failure)
+        "Type error!")
+      ast-or-failure)))
