1bc0d1978b07976f74e29f5e11c06dbd2b07f9a6
Luís Gabriel Lima <lampih@gmail.com>
2014-07-05 16:50:36 -0300

    E2: Initial version of the compiler
    Compiles E2 to Clojure.

---
+++ src/toy_languages/expressions2/compiler.clj
@@ -0,0 +1,67 @@
+(ns toy-languages.expressions2.compiler)
+
+(declare compile-exp)
+
+(defn- compile-value [value]
+  (nth (first value) 1))
+
+(defn- compile-id [subtree]
+  (symbol (first subtree)))
+
+(defn- to-vector-dec [decs]
+  (loop [ds decs v []]
+    (if (empty? ds)
+      v
+      (let [d (first ds)
+            const-id (first d)
+            exp (nth d 1)
+            id (symbol (nth const-id 1))
+            exp-code (compile-exp exp)
+            new-vec (conj (conj v id) exp-code)]
+        (recur (subvec ds 1) new-vec)))))
+
+(defn- compile-let [content]
+  (let [decs (first content)
+        exp (nth content 1)
+        let-decs (to-vector-dec decs)
+        exp-code (compile-exp exp)]
+    `(let ~let-decs ~exp-code)))
+
+(defn- compile-unary [exp]
+  (let [op (first exp)
+        subexp (nth exp 1)
+        exp-code (compile-exp subexp)]
+    (case op
+      :minus `(- 0 ~exp-code)
+      :not `(not ~exp-code)
+      :length `(.length ~exp-code))))
+
+(defn- compile-binary [exp]
+  (let [op (first exp)
+        lexp (nth exp 1)
+        rexp (nth exp 2)
+        lcode (compile-exp lexp)
+        rcode (compile-exp rexp)]
+    (case op
+      :add `(+ ~lcode ~rcode)
+      :minus `(- ~lcode ~rcode)
+      :and `(and ~lcode ~rcode)
+      :or `(or ~lcode ~rcode)
+      :equals `(= ~lcode ~rcode)
+      :concat `(concat ~lcode ~rcode))))
+
+(defn- compile-exp [exp]
+  (let [kind (first exp)
+        subtree (subvec exp 1)]
+    (case kind
+      :value (compile-value subtree)
+      :const-id (compile-id subtree)
+      :let-exp (compile-let subtree)
+      :unary-exp (compile-unary subtree)
+      :binary-exp (compile-binary subtree))))
+
+(defn compi [ast]
+  (let [node (first ast)
+        exp (nth ast 1)]
+    (case node
+      :program (compile-exp exp))))
